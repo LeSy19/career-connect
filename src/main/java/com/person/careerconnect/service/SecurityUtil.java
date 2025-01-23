f4e9abbc2e62f01b6ae806b433b5419cbdcad2c3
Le Van Sy <syle190104@gmail.com>
2025-01-23 23:13:28 +0700

    fix CORS, CRUD Company

--- src/main/java/com/person/careerconnect/service/SecurityUtil.java
+++ src/main/java/com/person/careerconnect/service/SecurityUtil.java
@@ -3,6 +3,9 @@ package com.person.careerconnect.service;
 import com.nimbusds.jose.util.Base64;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
 import org.springframework.security.oauth2.jwt.*;
 import org.springframework.stereotype.Service;
@@ -11,6 +14,7 @@ import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.Optional;
 
 @Service
 public class SecurityUtil {
@@ -44,5 +48,31 @@ public class SecurityUtil {
         JwsHeader jwsHeader = JwsHeader.with(JWT_ALGORITHM).build();
         return this.jwtEncoder.encode(JwtEncoderParameters.from(jwsHeader,
                 claims)).getTokenValue();
+    }
+
+    public static Optional<String> getCurrentUserLogin(){
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        return Optional.ofNullable(extractPrincipal(securityContext.getAuthentication()));
+    }
+
+    private static String extractPrincipal(Authentication authentication){
+        if(authentication == null){
+            return null;
+        }else if(authentication.getPrincipal() instanceof UserDetails springSecurityUser){
+            return springSecurityUser.getUsername();
+        }else if(authentication.getPrincipal() instanceof Jwt jwt){
+            return jwt.getSubject();
+        }else if(authentication.getPrincipal() instanceof String s){
+            return s;
+        }
+
+        return null;
+    }
+
+    public static Optional<String> getCurrentUserJwt(){
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        return Optional.ofNullable(securityContext.getAuthentication())
+                .filter(authentication -> authentication.getCredentials() instanceof String)
+                .map(authentication -> (String) authentication.getCredentials());
     }
 }
